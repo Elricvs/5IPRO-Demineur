70d8ffe4d05d00c8b3c03a736f0338b78ade4174
Manitob1 <damien.khashan@ifosup.wavre.be>
2023-03-12 22:15:44 +0100

    creation base

--- Van Steenwinckel Elric & Kashan Damien/MineSweeper.js
+++ Van Steenwinckel Elric & Kashan Damien/MineSweeper.js
@@ -1,43 +1,46 @@
+/* base/moteur du jeu */
+
 var MineSweeper = {
     name: 'MineSweeper',
-    
+
     difficulties: {
-    easy: {
-        lines: 8,
-        columns: 8,
-        mines: 10,
+        easy: {
+            lines: 8,
+            columns: 8,
+            mines: 10,
+        },
+        normal: {
+            lines: 12,
+            columns: 12,
+            mines: 20,
+        },
+        hard: {
+            lines: 16,
+            columns: 16,
+            mines: 32,
+        },
+        extreme: {
+            lines: 20,
+            columns: 20,
+            mines: 48,
+        },
     },
-    normal: {
-        lines: 12,
-        columns: 12,
-        mines: 20,
-    },
-    hard: {
-        lines: 16,
-        columns: 16,
-        mines: 32,
-    },
-    extreme: {
-        lines: 20,
-        columns: 20,
-        mines: 48,
-    },
-    },
-    
+
     settings: {
+
     },
-    
+
     game: {
         status: 0,
         field: new Array(),
     },
-    
+
     initialise: function() {
+        this.startGame('easy');
     },
-    
+
     startGame: function(difficulty) {
-    this.settings = this.difficulties[difficulty];
-    this.drawGameBoard();
-    this.resetGame();
+        this.settings = this.difficulties[difficulty];
+        this.drawGameBoard();
+        this.resetGame();
     },
-    }
