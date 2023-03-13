520b6b0975aa566890e7fe57598a7d502cbca645
Manitob1 <damien.khashan@ifosup.wavre.be>
2023-03-13 10:50:55 +0100

    ajout plateau de jeu

--- Van Steenwinckel Elric & Kashan Damien/MineSweeper.js
+++ Van Steenwinckel Elric & Kashan Damien/MineSweeper.js
@@ -44,3 +44,32 @@ var MineSweeper = {
         this.drawGameBoard();
         this.resetGame();
     },
+    drawGameBoard: function() {
+		/* récupère le plateau et le vider */
+        board = document.getElementById('plateau');
+        board.innerHTML = '';
+		/* vider la partie précédente */
+        document.getElementById('result').innerHTML = '';
+		/* bord de table et intérieur de la table + classe de style */
+        border = document.createElement('table');
+        border.setAttribute('oncontextmenu', 'return false;'); /* return false pour eviter un probleme sur le clic droit */
+        field = document.createElement('tbody');
+        border.appendChild(field);
+        border.className = 'field';
+		/* ajout du champ au plateau */
+        board.appendChild(border);
+		/* créé autant de ligne par rapport au settings */
+        for (i = 1; i <= this.settings['lines']; i++) {
+            line = document.createElement('tr');
+		/* créé les cellules + intéractions marquage d'une cellule ( mine? )*/
+            for (j = 1; j <= this.settings['columns']; j++) {
+                cell = document.createElement('td');
+                cell.id = 'cell-'+i+'-'+j;
+                cell.className = 'cell';
+                cell.setAttribute('onclick', this.name+'.checkPosition('+i+', '+j+', true);');
+                cell.setAttribute('oncontextmenu', this.name+'.markPosition('+i+', '+j+'); return false;');
+                line.appendChild(cell); 
+            }
+            field.appendChild(line);
+        }
+    },
